be672a939617f4e5aef297d02bb5103cbbe65705
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-12-17 04:22:53 -0800

    Update db_setup.sql

--- model/db_setup.sql
+++ model/db_setup.sql
@@ -37,7 +37,9 @@ CREATE TABLE `guestbook` (
 --
 -- Table structure for table `hashedVisitorIPs`
 --
+
 DROP TABLE IF EXISTS `hashedVisitorIPs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `hashedVisitorIPs` (
   `hashedVisitorIP` binary(32) NOT NULL,
