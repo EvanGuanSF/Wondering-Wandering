3813b2c989d6712a1b7eb95abcc36500089ab3ff
JammyPajamies <JammyPajamies@users.noreply.github.com>
2020-02-09 17:31:20 -0800

    Bug fixes and QoL changes for https-login integration.
    - Added production/dev certificate usage switching via ENV variable in .env file.
    - Updated to Nodejs 13 for main app.
    - Updated setup instructions for port routing, as manual routing is no longer really necessary with docker container port forwarding.

--- app.js
+++ app.js
@@ -21,16 +21,28 @@ app.set({
 app.disable('x-powered-by')
 
 // Create the https server.
-const openSSLCredentials = {
-  key: fs.readFileSync(path.join(__dirname, 'auth/key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'auth/cert.pem')),
-  passphrase: process.env.OPEN_SSL_PASSPHRASE
+var openSSLCredentials = ''
+if (process.env.ENV === 'dev') {
+  console.log('Application started in ' + process.env.ENV + ' configuration.')
+
+  openSSLCredentials = {
+    key: fs.readFileSync(path.resolve(process.env.OPEN_SSL_KEY_PATH.toString())),
+    cert: fs.readFileSync(path.resolve(process.env.OPEN_SSL_CERTIFICATE_PATH.toString())),
+    passphrase: process.env.OPEN_SSL_PASSPHRASE
+  }
+} else if (process.env.ENV === 'production') {
+  console.log('Application started in ' + process.env.ENV + ' configuration.')
+
+  openSSLCredentials = {
+    key: fs.readFileSync(path.resolve(process.env.OPEN_SSL_KEY_PATH.toString())),
+    cert: fs.readFileSync(path.resolve(process.env.OPEN_SSL_CERTIFICATE_PATH.toString()))
+  }
 }
 
 // Check all incoming traffic to see if it is connecting securely.
 app.all('*', ensureSecure)
 
-// Check the request and redirect to https if the user is connecting from unsecured http.
+// Check the request and redirect to https if the client is connecting from unsecured http.
 function ensureSecure (req, res, next) {
   if (req.protocol !== 'https') {
     // If the connection is an https connection, send it off to the appropriate route.
